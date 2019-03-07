f94a4a218044e92cdfc81d23f6cde4835cf49eb2
Kaidou <moya.arnaud.pro@gmail.com>
2019-03-07 14:59:10 +0100

    lol

--- pikaconnect/src/App.js
+++ pikaconnect/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Signup from './Component/SignUp'
-
 import './CSS/App.css';
 
 class App extends Component {
--- pikaconnect/src/Component/SignUp.js
+++ pikaconnect/src/Component/SignUp.js
@@ -1,23 +1,54 @@
 import React, { Component } from 'react';
 
+
+
+
 class SignUp extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
-            mail:"test@email.com"
+        this.state = {
+            mail: "test@email.com",
+            mdp: "t",
+            mdpverif: "t",
+            name: "t",
+            nickname: "t"
         }
     }
-  
-    updateEmailField = (event) => { 
-    
-this.setState({mail: event.target.value})
+
+    updateEmailField = (event) => {
+
+        this.setState({
+            mail: event.target.value
+        })
+    }
+
+    updateMdp = (event) => {
+        this.setState({
+            mdp: event.target.value
+        })
+    }
+    updateMdpverif = (event) => {
+        this.setState({
+            mdpverif: event.target.value
+        })
+    }
+    updateName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+    updateNickname = (event) => {
+        this.setState({
+            nickname: event.target.value
+        })
+    }
+    affichejson =()=> {
+        console.log(this.state)
+    }
 
 
-  }
-  
-  
-  
-  
+
+
     render() {
 
 
@@ -26,13 +57,20 @@ this.setState({mail: event.target.value})
 
         return (
             <div>
-                 <h1>{this.state.mail}</h1>
-                 <ul>
-                <li><input type="email" value={this.state.mail} name="email" onChange={this. updateEmailField}/></li>
-                 
-                 
-                </ul>
+                <h1>{JSON.stringify(this.state, 1, 1)}</h1>
+                <form>
+                    <input type="email" value={this.state.mail} name="email" onChange={this.updateEmailField} />
+                    <input type="mdp" value={this.state.mdp} name="Mdp" onChange={this.updateMdp} />
+                    <input type="mdpverif" value={this.state.mdpverif} name="Mdpverif" onChange={this.updateMdpverif} />
+                    <input type="name" value={this.state.name} name="Name" onChange={this.updateName} />
+                    <input type="nickname" value={this.state.nickname} name="Nickname" onChange={this.updateNickname} />
+                    <input type="button" value="Envoyer le formulaire" onClick={this.affichejson} />
+                </form>
+
+
+
             </div>
+
 
 
         )
